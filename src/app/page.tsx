4aa2214a2df1a121d693a7237cf3670cb5ce342a
Talasoftware <dtalahatu@trinitisolutions.nl>
2023-10-16 11:24:10 +0200

    app dispatch when dashboard is shown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import {useEffect} from "react";
+import {fetchEvents} from "@/redux/event/eventThunks";
+import {useAppDispatch} from "@/hooks/hooks";
 
 
 export default function Home() {
-
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchEvents())
+    }, [])
   return (
     <main >
       Dashboard
