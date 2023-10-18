c4ad4c6b5d2a391e3b45232b9c3a260d830834af
Talasoftware <dtalahatu@trinitisolutions.nl>
2023-10-18 03:31:13 +0200

    added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,25 @@
 import {useEffect} from "react";
 import {fetchEvents} from "@/redux/event/eventThunks";
 import {useAppDispatch} from "@/hooks/hooks";
+import {useUser} from "@auth0/nextjs-auth0/client";
 
 
 export default function Home() {
     const dispatch = useAppDispatch();
+    const { user, error, isLoading } = useUser();
     useEffect(() => {
-        dispatch(fetchEvents())
+
+            dispatch(fetchEvents())
+
     }, [])
+
+
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
   return (
     <main >
-      Dashboard
+       Welcome {user?.name}
     </main>
   )
 }
---
+++ src/app/profileDropDown.tsx
@@ -0,0 +1,62 @@
+import {Menu, Transition} from "@headlessui/react";
+import {ChevronDownIcon} from "@heroicons/react/20/solid";
+import React, {Fragment} from "react";
+import {useUser} from "@auth0/nextjs-auth0/client";
+import {useRouter} from "next/navigation";
+function classNames(...classes: any[]) {
+    return classes.filter(Boolean).join(' ')
+}
+export default function ProfileDropDown(){
+    const userNavigation = [
+        {name: 'Sign out', href: "/api/auth/logout"},
+    ]
+    const {user, error, isLoading} = useUser();
+    if(isLoading)
+        return <></>
+
+
+    return ( <Menu as="div" className="relative">
+        <Menu.Button className="-m-1.5 flex items-center p-1.5">
+            <span className="sr-only">Open user menu</span>
+            {user&&<img
+                className="h-8 w-8 rounded-full bg-gray-50"
+                src={user.picture}
+                alt={user.name}
+            />}
+            <span className="hidden lg:flex lg:items-center">
+                      <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+
+                      </span>
+                      <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true"/>
+                    </span>
+        </Menu.Button>
+        <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+        >
+            <Menu.Items
+                className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                {userNavigation.map((item) => (
+                    <Menu.Item key={item.name}>
+                        {({active}) => (
+                            <a
+                                href={item.href}
+                                className={classNames(
+                                    active ? 'bg-gray-50' : '',
+                                    'block px-3 py-1 text-sm leading-6 text-gray-900'
+                                )}
+                            >
+                                {item.name}
+                            </a>
+                        )}
+                    </Menu.Item>
+                ))}
+            </Menu.Items>
+        </Transition>
+    </Menu>);
+}
