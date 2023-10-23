6484cb9fc8c6b1934ca960382829ac0f9895d877
Talasoftware <dtalahatu@trinitisolutions.nl>
2023-10-23 15:14:53 +0200

    added cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-    projectId: 'r7jo2u',
-})
+  projectId: "r7jo2u",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
